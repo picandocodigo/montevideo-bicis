f4ca1394f1424f5f53ce9163681d22461fa053a5
Fernando Briano <fernando@picandocodigo.net>
2013-07-04 18:31:05 -0300

    Adds laws

--- index.rb
+++ index.rb
@@ -9,3 +9,12 @@ end
 get "/data" do
   haml :data, locals: {data: DeadPeople.new}
 end
+
+get "/leyes" do
+  haml :leyes
+end
+
+get "/leyes/:ley" do
+  ley = params[:ley]
+  haml :"leyes/#{ley}", layout: :layout
+end
