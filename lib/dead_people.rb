b869e1d0386cb596a45126f521bc52be774ee646
Fernando Briano <fernando@picandocodigo.net>
2013-07-04 01:36:16 -0300

    Adds raw data to data display

--- lib/dead_people.rb
+++ lib/dead_people.rb
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
 require 'csv'
 class DeadPeople
-  attr_reader :total, :bikes, :falls
+  attr_reader :total, :bikes, :falls, :accidents
 
   def initialize
     @total, @bikes, @falls = 0, 0, 0
+    @accidents = []
     get_bike_details
   end
 
   private
 
   def get_bike_details
-    CSV.foreach("data/fallecidos2012.csv") do |row|
+    CSV.foreach("data/fallecidos2012.csv", col_sep: ";") do |row|
+      @accidents << row
       @total += 1
-      @bikes += 1 if row[0] =~ /BICICLETA/
-      @falls += 1 if row[0] =~ /BICICLETA/ && row[0] =~ /CAIDA/
+      @bikes += 1 if row[3] =~ /BICICLETA/
+      @falls += 1 if row[3] =~ /BICICLETA/ && row[7] =~ /CAIDA/
     end
   end
 
