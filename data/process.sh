9fec0095c193412c1c7a98c6ef600396f004585b
Fernando Briano <fernando@picandocodigo.net>
2017-11-29 23:18:49 +0000

    Adds and processes data for 2016

--- data/process.sh
+++ data/process.sh
@@ -3,13 +3,4 @@ iconv -f ISO_8859-1 -t UTF-8 Personas_fallecidas_en_2012.csv > 2012.csv
 iconv -f ISO_8859-1 -t UTF-8 Fallecidos_2013.csv > 2013.csv
 iconv -f ISO_8859-1 -t UTF-8 Fallecidos_2014.csv > 2014.csv
 iconv -f ISO_8859-1 -t UTF-8 Fallecidos_2015.csv > 2015.csv
-
-
-
-
-
-
-
-
-
-
+iconv -f ISO_8859-1 -t UTF-8 Fallecidos_2016.txt | sed 's/\t/";"/g' | sed 's/\"//g' > 2016.csv
