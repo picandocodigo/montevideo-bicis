c32e2df9415f0993d7f03731d3124e3497282b44
Fernando Briano <fernando@picandocodigo.net>
2020-12-23 15:10:54 +0000

    Corrige JavaScript para menú en vista responsive

--- public/js/interactions.js
+++ public/js/interactions.js
@@ -1,42 +1,45 @@
-const years = ['2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012']
 document.addEventListener('DOMContentLoaded', () => {
-  years.forEach(function(year){
-    document.getElementById("btn_" + year).addEventListener("click", function(){
-      toggleVisible('table_' + year);
+  if(window.location.pathname.match(/accidentes/)){
+    const years = ['2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012']
+
+    years.forEach(function(year){
+      document.getElementById("btn_" + year).addEventListener("click", function(){
+        toggleVisible('table_' + year);
+      });
     });
-  });
 
-  function toggleVisible(id){
-    var thing =  document.getElementById(id);
-    if(thing.classList.contains("hidden")){
-      thing.className = "visible";
-    } else {
-      thing.className = "hidden";
-    }
-  }
-
-  var elements = document.querySelectorAll('.hiddable');
-  var length = elements.length;
-
-  for(var i = 0; i < length; i++){
-    elements[i].childNodes[1].addEventListener('click', function(){
-      var element = get_hiddable(this.parentNode);
-      if(element){
-        if(element.classList.contains('hidden')){
-          element.className = "hiddable_content visible";
-        } else {
-          element.className = "hiddable_content hidden";
-        }
+    function toggleVisible(id){
+      var thing =  document.getElementById(id);
+      if(thing.classList.contains("hidden")){
+        thing.className = "visible";
+      } else {
+        thing.className = "hidden";
       }
-    });
-  }
+    }
 
-  function get_hiddable(element){
-    var length = element.childElementCount;
-    var children = element.children;
+    var elements = document.querySelectorAll('.hiddable');
+    var length = elements.length;
+
     for(var i = 0; i < length; i++){
-      if (children[i].className.indexOf("hiddable_content") > - 1){
-        return children[i];
+      elements[i].childNodes[1].addEventListener('click', function(){
+        var element = get_hiddable(this.parentNode);
+        if(element){
+          if(element.classList.contains('hidden')){
+            element.className = "hiddable_content visible";
+          } else {
+            element.className = "hiddable_content hidden";
+          }
+        }
+      });
+    }
+
+    function get_hiddable(element){
+      var length = element.childElementCount;
+      var children = element.children;
+      for(var i = 0; i < length; i++){
+        if (children[i].className.indexOf("hiddable_content") > - 1){
+          return children[i];
+        }
       }
     }
   }
